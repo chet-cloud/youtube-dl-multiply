c952cd7147bb9b7ea11eee948e13b9d0f7514fda
chet <cchen@artisreit.com>
2022-09-07 22:56:33 -0500

    init

---
+++ download.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' https://www.youtube.com/playlist?list=PLj3-6Dw0D3Q6FpUD4mk0f-IBKT-B5eRFp
---
+++ download_all.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+xargs -n 1 -P 50 youtube-dl < my_video_links.txt
